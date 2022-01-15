43c3a119a9f997cd51fb46a99a969d29755703ad
zhuravlevma <monge.gaspard.1746@gmail.com>
2022-01-15 14:29:59 +0300

    add error to tcp library

---
+++ tcp/src/error.rs
@@ -0,0 +1,11 @@
+use std::io;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum ConnectError {
+    #[error("Unexpected handshake response: {0}")]
+    BadHandshake(String),
+
+    #[error("IO error: {0}")]
+    Io(#[from] io::Error)
+}
