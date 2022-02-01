57e6d65c159da23c06202931204766cb8c5dbe2d
zhuravlevma <monge.gaspard.1746@gmail.com>
2022-02-01 18:07:07 +0300

    add try_recv

--- smart_thermometer/src/server.rs
+++ smart_thermometer/src/server.rs
@@ -19,12 +19,32 @@ impl ThermometerServer {
 
     pub fn listen(self) {
         let arc_handle = Arc::new(self.handle);
+        let arc_connection = Arc::new(self.connection);
         let thread = thread::spawn(move || loop {
-            let (_number_of_bytes, src_addr, _data) = self.connection.receive();
+            let connection = arc_connection.clone();
+            let (_number_of_bytes, src_addr, _data) = connection.receive();
             let (tx, rx) = mpsc::channel();
             let handle = arc_handle.clone();
-            thread::spawn(move || tx.send(handle.routing(_data)).unwrap());
-            self.connection.response(rx.recv().unwrap(), src_addr);
+            thread::spawn(move || match tx.send(handle.routing(_data)) {
+                Ok(_) => {}
+                Err(error) => {
+                    println!("{}", error)
+                }
+            });
+            let connection = arc_connection.clone();
+            thread::spawn(move || {
+                loop {
+                    let result = rx.try_recv();
+                    match result {
+                        Ok(message) => {
+                            connection.response(message, src_addr);
+                        }
+                        Err(_error) => {
+                            // println!("not found");
+                        }
+                    }
+                }
+            });
         });
         thread.join().unwrap();
     }
