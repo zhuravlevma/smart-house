934ad487231bc72dc4e71e0085439eb6e88891d1
zhuravlevma <monge.gaspard.1746@gmail.com>
2022-02-06 01:41:58 +0300

    add example to smart_thermometer

---
+++ smart_thermometer/examples/thermometer_sync.rs
@@ -0,0 +1,11 @@
+use config::ConfigServer;
+use smart_thermometer::ThermometerServer;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let config = ConfigServer::new("127.0.0.1".to_string(), "8080".to_string())?;
+    let server = ThermometerServer::new(config);
+    server.listen();
+
+    Ok(())
+}
