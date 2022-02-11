b81d2bf93a925aed7144ebb53680aded2790e934
zhuravlevma <monge.gaspard.1746@gmail.com>
2022-02-12 00:13:04 +0300

    add actix_web server

--- http_server/src/main.rs
+++ http_server/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
+use actix_web::{App, HttpResponse, HttpServer};
+use log::LevelFilter;
+use std::error::Error;
+
+#[actix_web::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    // dotenv::dotenv()?;
+    env_logger::builder()
+        .filter_level(LevelFilter::Debug)
+        .init();
+
+    HttpServer::new(move || App::new().service(get_temperature))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await?;
     println!("Hello, world!");
+    Ok(())
+}
+
+#[actix_web::get("/temp")]
+async fn get_temperature() -> Result<HttpResponse, Box<dyn Error>> {
+    Ok(HttpResponse::Ok().json("Test result"))
 }
