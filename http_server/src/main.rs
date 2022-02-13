6a0420057ebdff2f093c7093d6e2f4b7cd04b2ee
zhuravlevma <monge.gaspard.1746@gmail.com>
2022-02-13 17:22:46 +0300

    add mongo house to http server

--- http_server/src/main.rs
+++ http_server/src/main.rs
@@ -1,18 +1,33 @@
-use actix_web::{App, HttpResponse, HttpServer};
+mod mongo;
+
+use crate::mongo::house::MongoHouse;
+use actix_web::web::Data;
+use actix_web::{web, App, HttpResponse, HttpServer};
 use log::LevelFilter;
+use std::env;
 use std::error::Error;
+use std::sync::Arc;
 
 #[actix_web::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    // dotenv::dotenv()?;
+    dotenv::dotenv()?;
     env_logger::builder()
         .filter_level(LevelFilter::Debug)
         .init();
 
-    HttpServer::new(move || App::new().service(get_temperature))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await?;
+    let mongo = MongoHouse::new(&env::var("MONGO_CONNECTION")?).await;
+
+    let houses_data = Arc::new(mongo);
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(Data::new(houses_data.clone()))
+            .service(get_temperature)
+            .service(get_houses)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await?;
     println!("Hello, world!");
     Ok(())
 }
@@ -20,4 +35,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
 #[actix_web::get("/temp")]
 async fn get_temperature() -> Result<HttpResponse, Box<dyn Error>> {
     Ok(HttpResponse::Ok().json("Test result"))
+}
+
+#[actix_web::get("/house")]
+async fn get_houses(houses: web::Data<Arc<MongoHouse>>) -> Result<HttpResponse, Box<dyn Error>> {
+    let boards = houses.get_houses().await?;
+    Ok(HttpResponse::Ok().json(boards))
 }
---
+++ http_server/src/mongo/house.rs
@@ -0,0 +1,32 @@
+use futures::StreamExt;
+use mongodb::bson::doc;
+use mongodb::bson::oid::ObjectId;
+use mongodb::Client;
+use serde::{Deserialize, Serialize};
+use std::error::Error;
+
+pub struct MongoHouse(Client);
+
+#[derive(Clone, Serialize, Deserialize)]
+pub struct HouseData {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    id: Option<ObjectId>,
+    name: String,
+}
+
+impl MongoHouse {
+    pub async fn new(connection_str: &str) -> Self {
+        Self(Client::with_uri_str(connection_str).await.unwrap())
+    }
+
+    pub async fn get_houses(&self) -> Result<Vec<HouseData>, Box<dyn Error>> {
+        let collection = self.0.database("smart_home").collection("house");
+        let query = doc! {};
+        let mut houses = collection.find(query, None).await?;
+        let mut houses_vec = Vec::new();
+        while let Some(board) = houses.next().await {
+            houses_vec.push(board?);
+        }
+        Ok(houses_vec)
+    }
+}
