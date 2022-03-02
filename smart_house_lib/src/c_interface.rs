d594b54f8fa95e6cd0543d3ebe41d30957aafae6
zhuravlevma <monge.gaspard.1746@gmail.com>
2022-03-02 14:30:45 +0300

    remove c libraries

--- smart_house_lib/src/c_interface.rs
+++
@@ -1,9 +0,0 @@
-// use crate::House;
-// use std::os::raw::c_char;
-//
-// pub(crate) type HouseFn = unsafe extern "C" fn(c_char, c_char) -> House;
-//
-// /// # Safety
-// pub unsafe extern "C" fn get_house(id: c_char, name: c_char) -> House {
-//     House::new(id.to_string(), name.to_string())
-// }
--- smart_house_lib/src/lib.rs
+++ smart_house_lib/src/lib.rs
@@ -7,11 +7,5 @@ pub use house::apartment::device::Device;
 pub use house::apartment::Apartment;
 pub use house::House;
 
-// #[no_mangle]
-// pub extern "C" fn get_my_integer() -> i32 {
-//     45
-// }
-
-pub mod c_interface;
 pub mod errors;
 pub mod house;
